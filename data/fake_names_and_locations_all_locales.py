420000291bed81264d4ad18c170cdfd1b6d5969f
Kathy Peacock <kathypeacockcreative@gmail.com>
2025-12-19 16:23:20 +0100

    data: updated generator py to include countries in relevant language ; replaced csv with new full dataset

--- data/fake_names_and_locations_all_locales.py
+++ data/fake_names_and_locations_all_locales.py
@@ -32,6 +32,40 @@ locales = [
     "zh_TW",
 ]
 
+# Map locales to their countries in native language
+locale_to_native_country = {
+    "bg_BG": "България",           # Bulgaria
+    "cs_CZ": "Česká republika",    # Czech Republic
+    "de_AT": "Österreich",         # Austria
+    "de_DE": "Deutschland",        # Germany
+    "dk_DK": "Danmark",            # Denmark
+    "el_GR": "Ελλάδα",            # Greece
+    "en_US": "United States",      # United States
+    "es_ES": "España",             # Spain
+    "es_MX": "México",             # Mexico
+    "fa_IR": "ایران",             # Iran 
+    "fi_FI": "Suomi",              # Finland
+    "fr_FR": "France",             # France
+    "hi_IN": "भारत",              # India 
+    "it_IT": "Italia",             # Italy
+    "ja_JP": "日本",               # Japan
+    "ko_KR": "대한민국",            # South Korea
+    "lv_LV": "Latvija",            # Latvia
+    "ne_NP": "नेपाल",              # Nepal
+    "nl_NL": "Nederland",          # Netherlands
+    "no_NO": "Norge",              # Norway
+    "pl_PL": "Polska",             # Poland
+    "pt_BR": "Brasil",             # Brazil
+    "pt_PT": "Portugal",           # Portugal
+    "ru_RU": "Россия",             # Russia
+    "sl_SI": "Slovenija",          # Slovenia
+    "sv_SE": "Sverige",            # Sweden
+    "tr_TR": "Türkiye",            # Turkey
+    "uk_UA": "Україна",            # Ukraine
+    "zh_CN": "中国",               # China 
+    "zh_TW": "台灣",               # Taiwan 
+}
+
 users = []
 
 with open("./names_and_cities.csv", "w") as f:
@@ -39,17 +73,21 @@ with open("./names_and_cities.csv", "w") as f:
 
 Faker.seed(12345)
 for locale in locales:
+    fake = Faker(locale)
+    country = locale_to_native_country.get(locale, "-")
+
     for _ in range(10):
-        fake = Faker(locale)
         name = fake.name()
         city = fake.city()
-        users.append({"name": name, "city": city})
+
+        users.append({"name": name, "city": city, "country": country})
         print(name)
         print(city)
+        print(country)
         print("")
-with open("./names_and_cities.csv", "a") as f:
+with open("./names_cities_countries.csv", "a") as f:
     for user in users:
         print(user)
-        line = f"{user['name']}, {user['city']}\n"
+        line = f"{user['name']}, {user['city']}, {user['country']}\n"
         f.write(line)
 
